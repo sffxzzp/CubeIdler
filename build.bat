dc2dc80a6385b868f45fbab7701576b48d570e6e
sffxzzp <sffxzzp@126.com>
2023-08-10 12:19:24 +0800

    update readme

--- build.bat
+++ build.bat
@@ -3,9 +3,10 @@ del /f /s /q .\*.upx
 del /f /q .\dist\*.exe
 cd runnerc
 g++ main.cpp -I include lib/cube_api64.lib -s -o runner
-upx -9 -vf --lzma --compress-icons=0 *.exe
 move *.exe ..\dist
 cd ..
 go build -ldflags="-s -w"
-upx -9 -vf --lzma --compress-icons=0 *.exe
 move *.exe .\dist
+cd dist
+upx --brute *.exe
+cd ..
