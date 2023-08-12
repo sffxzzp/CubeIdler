ea549d1dddbffb19743a79fe8740ae8c527b73d6
sffxzzp <sffxzzp@126.com>
2023-08-13 01:00:41 +0800

    compile runner staticly

--- runnerc/build32.bat
+++ runnerc/build32.bat
@@ -1,2 +1,2 @@
-g++ main.cpp -I include lib/cube_api.lib -s -m32 -O2 -o runner
+g++ main.cpp -I include lib/cube_api.lib -static -static-libgcc -s -m32 -O2 -o runner
 upx --brute *.exe
--- runnerc/build64.bat
+++ runnerc/build64.bat
@@ -1,2 +1,2 @@
-g++ main.cpp -I include lib/cube_api64.lib -s -O2 -o runner
+g++ main.cpp -I include lib/cube_api64.lib -static -static-libgcc -s -O2 -o runner
 upx --brute *.exe
